ec54ed92924e5b55937bae129867d6c18e379885
Will Lewis <will@wflewis.com>
2017-11-03 17:37:53 -0400

    Rearrange things to make library use possible

--- src/beeline.cr
+++ src/beeline.cr
@@ -1,33 +1,94 @@
-require "./beeline/*"
-
 STDIN.sync = true
 STDOUT.sync = true
 STDERR.sync = true
 
-module Beeline
-end
+class Beeline
+  VERSION = "0.1.0"
 
-Beeline::Beeline.config do
-  fore :black
-  back :yellow
-  padding
+  BASH_STOP_COUNTING  = "\x01"
+  BASH_START_COUNTING = "\x02"
+  ESCAPE              = "\x1B["
 
-  dirs = ENV["PWD"].split("/")[1..-1]
-  if dirs.size > 1
-    ellipsis
+  COLORS = {
+    black:       "0",
+    red:         "1",
+    green:       "2",
+    yellow:      "3",
+    blue:        "4",
+    magenta:     "5",
+    cyan:        "6",
+    white:       "7",
+    twofiftysix: "8",
+    clear:       "9",
+  }
+
+  @io : IO
+  @io = STDOUT
+
+  @fore : Symbol
+  @fore = :clear
+  @back : Symbol
+  @back = :clear
+
+  def self.config
+    b = Beeline.new
+    with b yield
+    b.reset
   end
-  print "/"
-  print dirs[-1]
 
-  padding
+  def fore(color)
+    @fore = color
+  end
 
-  fore :black
-  back :yellow
-  separator_empty
+  def back(color)
+    @back = color
+  end
 
-  fore :yellow
-  back :clear
-  separator
+  def padding
+    self << " "
+  end
 
-  separator_empty
+  def separator_empty
+    self << ""
+  end
+
+  def separator
+    self << ""
+  end
+
+  def ellipsis
+    self << "…"
+  end
+
+  def newline
+    self << "\n"
+  end
+
+  def print(thing)
+    self << thing
+  end
+
+  def <<(thing : String)
+    dont_count do
+      @io << escape("1;3" + COLORS[@fore] + "m")
+      @io << escape("1;4" + COLORS[@back] + "m")
+    end
+    @io << thing
+  end
+
+  def dont_count
+    @io << BASH_STOP_COUNTING
+    yield
+    @io << BASH_START_COUNTING
+  end
+
+  def escape(code : String)
+    ESCAPE + code
+  end
+
+  def reset
+    dont_count do
+      @io << escape("0m")
+    end
+  end
 end
